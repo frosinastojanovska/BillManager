6459963351e079d4a06dc88f12f3df263b508228
frosinastojanovska <stojanovska.frose@gmail.com>
2017-01-28 00:51:54 +0100

    Add procedures

--- BillManager.asm
+++ BillManager.asm
@@ -7,12 +7,31 @@ stack segment
 ends
 
 code segment
+    proc checkDate
+        
+    endp
+    
+    proc getIndex
+        
+    endp
+    
+    proc addBill
+    endp
+    
+    proc eraseBill
+        
+    endp
+    
+    proc maxBill
+        
+    endp
 start:
 ; set segment registers:
     mov ax, data
     mov ds, ax
     mov es, ax
 
+    ;vlez izlez kod tuka
     
 ;exit to operating system    
     mov ax, 4c00h
